a2a7334c7facfc0181ae0dd655b83ad29af1bd56
Kris Geusebroek <kgeusebroek@KgMBP2015.local>
2016-08-20 22:13:51 +0200

    Implement full load of tables

--- src/main/scala/nl/krisgeus/jdbc/load/raw/JdbcSourceLoader.scala
+++ src/main/scala/nl/krisgeus/jdbc/load/raw/JdbcSourceLoader.scala
@@ -1,29 +1,51 @@
 package nl.krisgeus.jdbc.load.raw
 
+import java.time.LocalDateTime
+
 import nl.krisgeus.Settings
+import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
+
 object JdbcSourceLoader {
+  val jdbcUrl = Settings().jdbcUrl
+  val driver = Settings().dbDriver
+
   def main(args: Array[String]): Unit = {
+    val now = LocalDateTime.now()
+
     val conf = new SparkConf().setAppName("Jdbc Source Loader")
     val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
 
     Settings().tableConfigs.foreach { table =>
       val name = table.getString("name")
-      val incremental = table.getBoolean("increment")
+      val incremental = table.getBoolean("incremental")
       val primarykeyColumns = table.getStringList("primarykey")
 
+      val tableDF: DataFrame = getTableAsDataframe(sqlContext, name)
+
+      tableDF.write.parquet(getOutputPath(now, name))
+
     }
+  }
+
+  def getOutputPath(now: LocalDateTime, name: String): String = {
+    f"/tmp/sourcedb/${name}/${now.getYear}/${now.getMonthValue}%02d/${now.getDayOfMonth}%02d/"
+  }
+
+  def getTableAsDataframe(sqlContext: SQLContext, tablename: String): DataFrame = {
+    sqlContext.read.format("jdbc").options(Map("url"->jdbcUrl, "dbtable"->s"${getLoadQuery(tablename)}", "driver"->driver)).load()
   }
 
   def getLoadQuery(tableName: String, incremental: Boolean = false, lastUpdateColumn: String = "", from: Long = -1, to: Long = -1): String = {
     incremental match {
-      case true => s"select * from ${tableName} where ${lastUpdateColumn} between ${from} and ${to}"
-      case false => s"select * from ${tableName}"
+      case true => s"(select * from ${tableName} where ${lastUpdateColumn} between ${from} and ${to}) ${tableName}"
+      case false => s"(select * from ${tableName}) ${tableName}"
     }
   }
 
   def getLoadPrimarykeysQuery(tableName: String, primaryKeys: List[String]): String = {
-    s"select ${primaryKeys.mkString(",")} from ${tableName}"
+    s"(select ${primaryKeys.mkString(",")} from ${tableName}) ${tableName}"
   }
 }
--- src/test/scala/nl/krisgeus/jdbc/load/raw/JdbcSourceLoaderTest.scala
+++ src/test/scala/nl/krisgeus/jdbc/load/raw/JdbcSourceLoaderTest.scala
@@ -1,18 +1,25 @@
 package nl.krisgeus.jdbc.load.raw
 
+import java.time.{LocalDateTime, Month}
 import org.scalatest.{DiagrammedAssertions, FunSuite}
 
 class JdbcSourceLoaderTest extends FunSuite with DiagrammedAssertions {
 
   test("determine load query based on tablename only") {
-    assert(JdbcSourceLoader.getLoadQuery("table1") === "select * from table1")
+    assert(JdbcSourceLoader.getLoadQuery("table1") === "(select * from table1) table1")
   }
 
   test("determine load query based on tablename with increments") {
-    assert(JdbcSourceLoader.getLoadQuery("table1", incremental = true, "lastUpdated", 10L, 20L) === "select * from table1 where lastUpdated between 10 and 20")
+    assert(JdbcSourceLoader.getLoadQuery("table1", incremental = true, "lastUpdated", 10L, 20L) === "(select * from table1 where lastUpdated between 10 and 20) table1")
   }
 
   test("determine load query for keys") {
-    assert(JdbcSourceLoader.getLoadPrimarykeysQuery("table1", List("col1","col2")) === "select col1,col2 from table1")
+    assert(JdbcSourceLoader.getLoadPrimarykeysQuery("table1", List("col1","col2")) === "(select col1,col2 from table1) table1")
+  }
+
+  test("output path creation") {
+    val dt: LocalDateTime = LocalDateTime.of(2016, Month.APRIL, 21, 0, 0, 0)
+    val result: String = JdbcSourceLoader.getOutputPath(dt, "table1")
+    assert(result === "/tmp/sourcedb/table1/2016/04/21/")
   }
 }

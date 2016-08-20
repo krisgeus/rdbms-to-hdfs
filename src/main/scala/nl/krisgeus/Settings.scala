ed26b703a50b5ffc9fa2434cd7082da5e58a8573
Kris Geusebroek <kgeusebroek@KgMBP2015.local>
2016-08-20 22:13:27 +0200

    Add database configuration config values

--- src/main/scala/nl/krisgeus/Settings.scala
+++ src/main/scala/nl/krisgeus/Settings.scala
@@ -5,6 +5,17 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 class Settings(config: Config) {
 
+  val dbName = config.getString("jdbc.sourcedb.db.name")
+  val dbUser = config.getString("jdbc.sourcedb.db.user")
+  val dbPwd  = config.getString("jdbc.sourcedb.db.pwd")
+  val dbDriver = config.getString("jdbc.sourcedb.db.driver")
+  val dbType = config.getString("jdbc.sourcedb.db.type")
+  val dbHost = config.getString("jdbc.sourcedb.db.host")
+  val dbPort = config.getInt("jdbc.sourcedb.db.port")
+
+
+  val jdbcUrl = s"jdbc:${dbType}://${dbHost}:${dbPort}/${dbName}?user=${dbUser}&password=${dbPwd}&serverTimezone=UTC"
+
   val tableConfigs: List[Config] = config.getConfigList("jdbc.sourcedb.tables").asScala.toList
 }
 
